cd0b4b6c9d577c8a4d43c53abf8e2cc65ec8645b
Sudipta Mitra <109846116+SmitraHub@users.noreply.github.com>
2024-10-17 10:02:03 +0530

    Update DistanceMetric.py

--- Scripts/new/DistanceMetric.py
+++ Scripts/new/DistanceMetric.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy as sp
 from wepy.resampling.distances.distance import Distance
-from new.Features import SelectedAtomsPairwise_distance, All_CACAdistance
+from new.features import SelectedAtomsPairwise_distance, All_CACAdistance
 
 
 

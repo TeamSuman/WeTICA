c6fa7eb2eed50e44f761c48845a4c61e69ac836b
Sudipta Mitra <109846116+SmitraHub@users.noreply.github.com>
2024-10-21 11:52:04 +0530

    Update revo2.py

--- Scripts/new/revo2.py
+++ Scripts/new/revo2.py
@@ -224,7 +224,7 @@ class REVOResampler(CloneMergeResampler):
                 # re-determine variation function, and walker_variations values
                 new_variation, walker_variations = self._calcvariation(new_walker_weights, new_num_walker_copies, distance_arr)
 
-                if new_variation >= variation:
+                if new_variation > variation:
                     variations.append(new_variation)
 
                     logging.info("Variance move to {} accepted".format(new_variation))
